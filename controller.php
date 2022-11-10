8ec8809b9bfd228d01368868fc83e2de263ee90d
Robert Göpfert <robert.goepfert1999@gmail.com>
2022-11-10 20:14:07 +0100

    +SQL-Injection +Datenbank zurücksetzen +Tabellen anzeigen +erste Styles ~Datenbankverbindung angepasst
    Co-authored-by: Dr-PPSN <Dr-PPSN@users.noreply.github.com>

--- controller.php
+++ controller.php
@@ -1,17 +1,111 @@
 <?php
-// Server Settings
-$servername = "db5010772275.hosting-data.io";
-$username = "dbu1627740";
-$password = "Me@Og_oB5f3fb5c";
-// Database Name
-$database = "dbs9113196";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
+require_once 'konfig.php';
+$conn = new mysqli(MYSQL_HOST, MYSQL_BENUTZER, MYSQL_KENNWORT);
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+	die("Connection failed: " . $conn->connect_error);
 }
 
-?>
+$notification = '';
+
+$selTable = getSelTable();
+
+$table = buildHtmlTable(getTableData('AUTOREN'));
+if (!empty($_POST['btnGetTable'])) {
+	$table = buildHtmlTable(getTableData($_POST['selTable']));
+}
+if (!empty($_POST['btnSQLInjection'])) {
+  $table = buildHtmlTable(executeSQL($_POST['sql-injection-text']));
+}
+
+if (!empty($_POST['btnReset'])) {
+	resetDB();
+}
+
+function getSelTable() {
+	$tableData = executeSQL("select TABLE_NAME from information_schema.tables where table_type = 'BASE TABLE' and TABLE_SCHEMA = 'buchladen';");
+	if (isset($tableData)) {
+		$htmlString = '<select name="selTable" id="selTable">';
+		for ($i = 1; $i < count($tableData); $i++) {
+      $row = $tableData[$i];
+			foreach ($row as $value) {
+        if (isset($_POST['selTable']) && $value == $_POST['selTable']) {
+          $htmlString .= '<option value="' . $value . '" selected>' . strtoupper($value) . '</option>';
+        } else {
+          $htmlString .= '<option value="' . $value . '">' . strtoupper($value) . '</option>';
+        }
+      }
+		}
+		$htmlString .= '</select>';	
+		return $htmlString;
+	}
+}
+
+function getTableData($table) {
+	return executeSQL("SELECT * FROM buchladen." . $table . ";");
+}
+
+function executeSQL($SQL) {
+	global $conn;
+  	global $notification;
+	$result = $conn->query($SQL);
+  if ($result === true) {
+    return true;
+  } else if ($result === false) {
+    $notification = 'Ups ein Fehler ist aufgetreten';
+    return false;
+  } else {
+    $tableData[] = $result->fetch_fields();
+    while ($row = $result->fetch_assoc()) {
+      $tableData[] = $row;
+    }
+    return $tableData;
+  }
+}
+
+function buildHtmlTable($tabledata) {
+	if (isset($tabledata)) {
+		$tableHeader = $tabledata[0];
+		$thHTML = '<tr>';
+		foreach ($tableHeader as $value) {
+			$thHTML .= '<th scope="col">' . $value->name . '</th>';
+		}
+		$thHTML .= '</tr>';
+
+    $trHTML = '';
+		for ($i = 1; $i < count($tabledata); $i++) {
+			$row = $tabledata[$i];
+			$trHTML .= '<tr>';
+			foreach ($row as $value) {
+				$trHTML .= '<td>' . $value . '</td>';
+			}
+			$trHTML .= '</tr>';
+		}
+		$tableHTML = '<table class="table table-striped table-dark">' . $thHTML . $trHTML . '</table>';
+	}
+	return $tableHTML;
+}
+
+function resetDB() {
+	global $conn;
+  global $notification;
+	if (file_exists('./buchladen.sql') == true) {
+		$filename = './buchladen.sql';
+	} else {
+		$notification = 'Error: Keine Datenbankdatei gefunden';
+    return;
+	}
+
+	$tempLine = '';
+	$lines = file($filename);
+	foreach ($lines as $line) {
+		if (substr($line, 0, 2) == '--' || $line == '')
+			continue;
+		$tempLine .= $line;
+		if (substr(trim($line), -1, 1) == ';') {
+			mysqli_query($conn, $tempLine) or print("Error in " . $tempLine . ":" . mysqli_error($conn));
+			$tempLine = '';
+		}
+	}
+  $notification = 'Tables imported successfully';
+}
--- index.php
+++ index.php
@@ -7,17 +7,107 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
         <meta charset="utf-8">
-        <link rel="shortcut icon" href="assets/img/icon.png">
         <title>BBS PHP Projekt</title>
-        <link rel="stylesheet" type="text/css" href="assets/css/index.css">
-        <link rel="stylesheet" type="text/css" href="assets/css/cookie_banner.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="message.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <div class="container-fluid bg-danger">
+        <div class="container-fluid h-100 bg-dark">
+            <!-- Menue -->
+            <div class="row bg-dark">
+                <div class="col-md-7 col-sm-6 col-xs-4 py-4 h1 d-flex align-items-center justify-content-center neonTextFlickerGreen">
+                    PHP Projekt Buchladen
+                </div>
+                <div class="col-md-5 col-sm-6 col-xs-8 d-flex align-items-center justify-content-center">
+                    <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
+                        <a class="btn btn-danger align-items-center justify-content-center" data-toggle="modal" data-target="#sql_injection_modal">
+                            SQL-Injection
+                        </a>
+                    </div>
+                    <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
+                        <form action="" method="post">
+                            <button type="submit" name="btnReset" class="btn btn-danger" value="reset">Datenbank zurücksetzen</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- /Menue -->
+            <!-- Row1 -->
+            <div class="row bg-dark py-3">
+                <div class="col-md-6 col-sm-6 col-xs-6 h3 d-flex align-items-center justify-content-center neonTextRed">
+                    Datenbank auswählen:
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
+                    <form action="" method="post">
+                        <?php echo $selTable?>
+                        <button type="submit" name="btnGetTable" class="button" id="btnAnzeigen" value="getTable">Anzeigen</button>
+                    </form>
+                </div>
+            </div>
+            <!-- /Row1 -->
+            <!-- Die ausgewaelte Tabelle -->
+            <div class="row bg-dark">
+                <div class="col-md-12 d-flex align-items-center justify-content-center" id="tableElement">
+                    <?php echo $table?>
+                </div>
+            </div>
+            <!-- /Die ausgewaelte Tabelle -->
+            <form action="" method="post">
+                <label for="buecher_Id">Buch ID: </label><br>
+                <input type="text" name="buecher_Id"><br>
+                <label for="titel">Titel: </label><br>
+                <input type="text" name="titel"><br>
+                <button type="submit" name="add" class="button" value="add">Einfügen</button>
+            </form>
             
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        <script src="https://code.jquery.com/jquery-3.5.1.min.js"   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="   crossorigin="anonymous"></script>
+        <!-- Modal SQL-Injection -->
+        <div class="modal fade" id="sql_injection_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalCenterTitle">Auszuführendes SQL hier eingeben</h5>
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">X</button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="sql-injection-text" name="sql-injection-text" placeholder="SQL">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <input type="submit" class="btn btn-success" name="btnSQLInjection" id="sql-injection-senden" value="SQL Senden">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Modal SQL-Injection -->
+        <!-- import scripts -->
+        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        <script>
+            function displayMessage(message){
+                if (!message==""){
+                    var body = $("body");
+                    $(body).append("<div id='message_div'></div>")
+                    $("#message_div").append("<div>" + message + "</div>");
+                    setTimeout(
+                        function() {
+                            $("#message_div").fadeOut(1000);
+                        }, 3000);
+                        setTimeout(
+                            function() {
+                            $("#message_div").remove();
+                            }, 6000);
+                }
+            }
+            $( document ).ready(function() {
+                if($("#tableElement").text().length > 0){
+                    //$("#btnAnzeigen").click();
+                }
+            });
+        </script>
+        <?php echo '<script>displayMessage("' . $notification . '");</script>' ?>
     </body>
 </html>
