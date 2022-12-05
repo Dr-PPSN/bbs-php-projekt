4cb66a334b7536e7f05529a3fa56d7398c851143
Robert Göpfert <robert.goepfert1999@gmail.com>
2022-12-05 23:32:15 +0100

    ~ format

--- index.php
+++ index.php
@@ -30,53 +30,53 @@ function checkPHPVersion() {
   </head>
   <body>
     <div class="container-fluid h-100 bg-dark">
-          <!-- Menue -->
-          <div class="row bg-dark">
-              <div class="col-md-7 col-sm-6 col-xs-4 py-4 h1 d-flex align-items-center justify-content-center neonTextFlickerGreen">
-                  PHP Projekt Buchladen
-              </div>
-              <div class="col-md-5 col-sm-6 col-xs-8 d-flex align-items-center justify-content-center">
-                  <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
-                      <a class="btn neon-button" data-toggle="modal" data-target="#sql_injection_modal">
-                          SQL-Injection
-                      </a>
-                  </div>
-                  <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
-                      <form action="" method="post">
-                          <button type="submit" name="btnReset" class="btn neon-button" value="reset">Datenbank zurücksetzen</button>
-                      </form>
-                  </div>
-              </div>
+      <!-- Menue -->
+      <div class="row bg-dark">
+        <div class="col-md-7 col-sm-6 col-xs-4 py-4 h1 d-flex align-items-center justify-content-center neonTextFlickerGreen">
+          PHP Projekt Buchladen
+        </div>
+        <div class="col-md-5 col-sm-6 col-xs-8 d-flex align-items-center justify-content-center">
+          <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
+            <a class="btn neon-button" data-toggle="modal" data-target="#sql_injection_modal">
+              SQL-Injection
+            </a>
           </div>
-          <!-- /Menue -->
-          <!-- Row1 -->
-          <div class="row bg-dark py-3">
-            <div class="col-md-6 col-sm-6 col-xs-6 h3 d-flex align-items-center justify-content-center neonTextRed">
-              Datenbank auswählen:
-            </div>
-            <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
-              <!-- TODO: Button für Tabellenauswahl hinzufügen -->
-              <form action="pages/table.php" method="get">
-                <button type="submit" name="table" class="button" id="btnTable" value="buecher">Bücher</button>
-                <button type="submit" name="table" class="button" id="btnTable" value="autoren">Autoren</button>
-                <button type="submit" name="table" class="button" id="btnTable" value="sparten">Sparten</button>
-                <button type="submit" name="table" class="button" id="btnTable" value="verlage">Verlage</button>
-                <button type="submit" name="table" class="button" id="btnTable" value="lieferanten">Lieferanten</button>
-                <button type="submit" name="table" class="button" id="btnTable" value="orte">Orte</button>
-              </form>
-              <form action="pages/alltables.php" method="get">
-                <button type="submit" name="alltables" class="button" id="btnAllTables">Alle Tabellen anzeigen</button>
-              </form>
-              <form action="pages/sqlinjection.php" method="post">
-                <button type="submit" name="sqlinjection" class="button" id="btnSQLInjection">SQL Injection</button>
-              </form>
-              <form action="" method="post">
-                <button type="submit" name="btnReset" class="button" id="btnReset" value="">Datenbank zurücksetzen</button>
-              </form>
-            </div>
+          <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
+            <form action="" method="post">
+              <button type="submit" name="btnReset" class="btn neon-button" value="reset">Datenbank zurücksetzen</button>
+            </form>
           </div>
-          <!-- /Row1 -->
+        </div>
       </div>
+      <!-- /Menue -->
+      <!-- Row1 -->
+      <div class="row bg-dark py-3">
+        <div class="col-md-6 col-sm-6 col-xs-6 h3 d-flex align-items-center justify-content-center neonTextRed">
+          Datenbank auswählen:
+        </div>
+        <div class="col-md-6 col-sm-6 col-xs-6 d-flex align-items-center justify-content-center">
+          <!-- TODO: Button für Tabellenauswahl hinzufügen -->
+          <form action="pages/table.php" method="get">
+            <button type="submit" name="table" class="button" id="btnTable" value="buecher">Bücher</button>
+            <button type="submit" name="table" class="button" id="btnTable" value="autoren">Autoren</button>
+            <button type="submit" name="table" class="button" id="btnTable" value="sparten">Sparten</button>
+            <button type="submit" name="table" class="button" id="btnTable" value="verlage">Verlage</button>
+            <button type="submit" name="table" class="button" id="btnTable" value="lieferanten">Lieferanten</button>
+            <button type="submit" name="table" class="button" id="btnTable" value="orte">Orte</button>
+          </form>
+          <form action="pages/alltables.php" method="get">
+            <button type="submit" name="alltables" class="button" id="btnAllTables">Alle Tabellen anzeigen</button>
+          </form>
+          <form action="pages/sqlinjection.php" method="post">
+            <button type="submit" name="sqlinjection" class="button" id="btnSQLInjection">SQL Injection</button>
+          </form>
+          <form action="" method="post">
+            <button type="submit" name="btnReset" class="button" id="btnReset" value="">Datenbank zurücksetzen</button>
+          </form>
+        </div>
+      </div>
+      <!-- /Row1 -->
+    </div>
     <!-- Modal SQL-Injection -->
       <div class="modal fade" id="sql_injection_modal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
           <div class="modal-dialog modal-dialog-centered" role="document">
