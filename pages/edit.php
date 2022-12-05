220f867f2bc9d841ce1412b32a984a55d06b7a08
Robert Göpfert <robert.goepfert1999@gmail.com>
2022-12-05 22:33:44 +0100

    ~ Format: Einzug nur 2 Spaces

--- pages/edit.php
+++ pages/edit.php
@@ -40,28 +40,28 @@ function buildEditForm($id) {
 
 <!DOCTYPE html>
 <html lang=de>
-    <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-        <meta charset="utf-8">
-        <title>BBS PHP Projekt</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="./styles/message.css">
-        <link rel="stylesheet" type="text/css" href="./styles/style.css">
-    </head>
-    <body>
-        <div class="container-fluid h-100 bg-dark">
-          <!-- Die ausgewaelte Tabelle -->
-          <div class="row bg-dark">
-              <div class="col-md-12 d-flex align-items-center justify-content-center" id="tableElement">
-                  <?php echo $editHTML?>
-              </div>
-          </div>
-          <!-- /Die ausgewaelte Tabelle -->
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    <meta charset="utf-8">
+    <title>BBS PHP Projekt</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./styles/message.css">
+    <link rel="stylesheet" type="text/css" href="./styles/style.css">
+  </head>
+  <body>
+    <div class="container-fluid h-100 bg-dark">
+      <!-- Die ausgewaelte Tabelle -->
+      <div class="row bg-dark">
+        <div class="col-md-12 d-flex align-items-center justify-content-center" id="tableElement">
+          <?php echo $editHTML?>
         </div>
-        <!-- import scripts -->
-        <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-        <script src="./js/notification.js"></script>
-    </body>
+      </div>
+      <!-- /Die ausgewaelte Tabelle -->
+    </div>
+    <!-- import scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="./js/notification.js"></script>
+  </body>
 </html>
