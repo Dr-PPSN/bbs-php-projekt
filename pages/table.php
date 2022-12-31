cece4770e3b0542dc11db16bca76b5b0ca103728
Robert Göpfert <robert.goepfert1999@gmail.com>
2022-12-31 14:55:59 +0100

    ~ format

--- pages/table.php
+++ pages/table.php
@@ -133,12 +133,12 @@ $deletePopupHTML  = getDeletePopup($selectedTable, $rows);
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <script src="../js/notification.js"></script>
     <script>
-      $("#ueberschrift").click(function(){
+      $("#ueberschrift").click(function() {
         window.location.href = "../index.php";
       });
     </script>
     <?php
-      if (isset($notification)){
+      if (isset($notification)) {
         echo '<script>displayMessage("' . $notification . '");</script>';
       }
     ?>
